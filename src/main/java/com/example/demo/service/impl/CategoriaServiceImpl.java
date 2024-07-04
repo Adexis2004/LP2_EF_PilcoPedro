6d6369c2499ae32d80f1463be90727a4a7415cb2
Adexis2004 <junpilco12@gmail.com>
2024-07-04 02:38:41 -0500

    Realizando los ServiceImpl

---
+++ src/main/java/com/example/demo/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.CategoriaEntity;
+import com.example.demo.repository.CategoriaRepository;
+import com.example.demo.service.CategoriaService;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public List<CategoriaEntity> findAllCategorias() {
+		return categoriaRepository.findAll();
+	}
+
+	@Override
+	public CategoriaEntity findCategoriaById(Integer id) {
+		return categoriaRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public CategoriaEntity saveCategoria(CategoriaEntity categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	@Override
+	public void deleteCategoria(Integer id) {
+		categoriaRepository.deleteById(id);
+
+	}
+
+}
